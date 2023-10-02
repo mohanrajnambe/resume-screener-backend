96c69c9f63f901c527326c4e7054efa8a9058265
deva <ds@presidio.com>
2023-10-02 23:12:21 +0530

    Updated relavency score

--- cron/process_context.py
+++ cron/process_context.py
@@ -2,6 +2,7 @@ import utils.dynamodb_utils as dynamoDbUtils
 import utils.lambda_utils as lambdaUtils
 import requests
 import json
+import ast
 
 def getNoRelevencyScoreData():
     response = requests.get('https://qvizwfql6f.execute-api.us-east-1.amazonaws.com/dev/application-no-relevancy')
@@ -26,13 +27,15 @@ def run():
                 responseJson = json.loads(response['response'])
                 updateSummary = dynamoDbUtils.putResumeSummary(str(candidate['candidate_id']), str(candidate['job_id']), json.dumps(responseJson['resume']))
                 if (updateSummary['ResponseMetadata']['HTTPStatusCode'] == 200):
+                    relevancy = ast.literal_eval(responseJson['relevancy'])
                     requestBody = {
                         'jobid': str(candidate['job_id']),
                         'candidateid': str(candidate['candidate_id']),
-                        'percentageslist': responseJson['relevancy']
+                        'percentageslist': relevancy
                     }
                     updateRelevency = requests.post(
-                        'https://qvizwfql6f.execute-api.us-east-1.amazonaws.com/dev/application-no-relevancy',
-                        data=requestBody
+                        'https://qvizwfql6f.execute-api.us-east-1.amazonaws.com/dev/post-average-relevancy',
+                        data=json.dumps(requestBody)
                     )
+    print('Context Processed')
     return None
